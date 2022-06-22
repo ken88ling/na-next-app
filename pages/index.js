522fb81171d8deff90a0f4f9113c7a6e0fcffe11
daviv <1073113@gmail.com>
2022-06-22 03:49:24 +0000

    Update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to David Ho Webside with Next.js!
+          Welcome to David Ho Website with Next.js!
         </h1>
 
         <p className={styles.description}>
